6a45a74cf88e0b2a4def40a0d1abb044e0d249a0
yiyu0x <yiyu0x@icloud.com>
2020-08-15 13:28:26 +0800

    Fix zbackup.sh error

--- sa-pre-pratice/hw3/zbackup.sh
+++ sa-pre-pratice/hw3/zbackup.sh
@@ -1,19 +1,45 @@
 #!/bin/sh
 
 list() {
-    zfs list -t snapshot -o name,creation -s creation | \
-        awk '{if(NR>1) print $1}' | \
-        awk -F "@" 'BEGIN {printf "%-3s %-20s %-20s\n", "ID", "DATASET", "TIME"} 
-            {printf "%-3s %-20s %-20s\n", NR ,$1, $2}'
+    if [ -z $1 ];then #Not other argv, only has -l 
+        zfs list -t snapshot -o name,creation -s creation | \
+            awk '{if(NR>1) print $1}' | \
+            awk -F "@" 'BEGIN {printf "%-3s %-20s %-20s\n", "ID", "DATASET", "TIME"}
+                {printf "%-3s %-20s %-20s\n", NR, $1, $2}'
+    else
+        echo $1 | grep -q "^[0-9]*$"
+        if [ $? = 0 ];then #-l and ID is set
+                zfs list -t snapshot -o name,creation -s creation | \
+                    awk -v id=$1 '{if(NR-1==id) print $1}' | \
+                    awk -v id=$1 -F "@" 'BEGIN {printf "%-3s %-20s %-20s\n", "ID", "DATASET", "TIME"}
+                        {printf "%-3s %-20s %-20s\n", id, $1, $2}'
+        else #-l and dataset is set
+            dataset=$1
+            if [ -z $2 ];then #Not set dataset ID
+                zfs list -t snapshot -o name,creation -s creation | \
+                    grep $dataset |\
+                    awk '{print $1}' | \
+                    awk -F "@" 'BEGIN {printf "%-3s %-20s %-20s\n", "ID", "DATASET", "TIME"}
+                        {printf "%-3s %-20s %-20s\n", NR, $1, $2}'
+            else
+                id=$2
+                zfs list -t snapshot -o name,creation -s creation | \
+                    grep $dataset |\
+                    awk -v id=$id '{if(NR==id) print $1}' | \
+                    awk -v id=$id -F "@" 'BEGIN {printf "%-3s %-20s %-20s\n", "ID", "DATASET", "TIME"}
+                        {printf "%-3s %-20s %-20s\n", id, $1, $2}'
+            fi
+        fi
+    fi
 }
 
 delete() {
-    echo $1 | grep -q "^[0-9]*$"
     if [ -z $1 ];then #Not other argv, only has -d
-        list | awk '{print $2"@"$3}' | xargs -L1 echo Destroy
-        list | awk '{print $2"@"$3}' | xargs -L1 zfs destroy
+        list | awk '{if(NR>1) print $2"@"$3}' | xargs -L1 echo Destroy
+        list | awk '{if(NR>1) print $2"@"$3}' | xargs -L1 zfs destroy
     else
-	    if [ $? = 0 ];then
+        echo $1 | grep -q "^[0-9]*$"
+	    if [ $? = 0 ];then #Specific ID
 	        target_name=$(list | awk NR==$1+1'{print}' | awk '{print $2"@"$3}')
 	        zfs destroy $target_name
 	        echo "Destroy $target_name"
@@ -21,7 +47,7 @@ delete() {
 	        if [ -z $2 ];then #Only specific dataset
 	            list | grep $1 | awk '{print $2"@"$3}' | xargs -L1 echo Destroy
 	            list | grep $1 | awk '{print $2"@"$3}' | xargs -L1 zfs destroy
-	        else
+	        else #Specific dataset and ID
                 list | grep $1 | awk '{print $2"@"$3}' | awk "{if(NR==$2) print}" | xargs -L1 echo Destroy
 	            list | grep $1 | awk '{print $2"@"$3}' | awk "{if(NR==$2) print}" | xargs -L1 zfs destroy
 	        fi
@@ -37,18 +63,23 @@ zexport() {
         id=$2
     fi
     target_name=$(list | grep $dataset | awk '{print $2"@"$3}' | awk "{if(NR==$id) print}")
-    zfs send $target_name | gzip > /tmp/backupfile.gz
+    tmpfile=$(mktemp /tmp/backup.gz.XXX)
+    zfs send $target_name | gzip > $tmpfile
     save_target_name=$(echo $target_name | tr "/" "-")
-    openssl aes-256-cbc -a -salt -in /tmp/backupfile.gz -out "/tmp/$save_target_name.gz.enc"
+    openssl aes-256-cbc -a -salt -pbkdf2 -in $tmpfile -out "/tmp/$save_target_name.gz.enc"
     echo "Export $target_name to /tmp/$save_target_name.gz.enc"
+    rm $tmpfile
 }
 
 import() {
     filename=$1
     dataset=$2
-    openssl aes-256-cbc -d -a -in $filename -out /tmp/backupfile.gz
-    gunzip /tmp/backupfile.gz
-    zfs receive -F $dataset < /tmp/backupfile
+    tmpfile=$(mktemp -u /tmp/backup.XXX)
+    tmpfilegz="$tmpfile.gz"
+    openssl aes-256-cbc -d -a -pbkdf2 -in $filename -out $tmpfilegz
+    gunzip $tmpfilegz
+    zfs receive -F $dataset < $tmpfile
+    rm $tmpfile
 }
 
 create() {
@@ -69,7 +100,7 @@ create() {
 
 argv=$1
 case $argv in
-    -l) list;;
+    -l) list $2 $3;;
     -d) delete $2 $3;;
     -e) zexport $2 $3;;
     -i) import $2 $3;;
